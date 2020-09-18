6258ff559689d29af838d4cdba9fb0e695e07ca1
Paul Hagwood <SubtleKnife@Pauls-MBP.attlocal.net>
2020-09-18 18:32:42 -0400

    reworked js to include questions

--- script.js
+++ script.js
@@ -1,64 +1,177 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // set an object passValues with all the variables needed for the password
-var passValues = {
-  alphabet: [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-  ],
-  number: [ 
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-  ],
-  specialChar: [
-    "@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~", "`","_", "."
-  ]
-}
+
+var lowCaseAlphabet = [
+  "a", 
+  "b", 
+  "c", 
+  "d", 
+  "e", 
+  "f", 
+  "g", 
+  "h", 
+  "i", 
+  "j", 
+  "k", 
+  "l", 
+  "m", 
+  "n", 
+  "o", 
+  "p", 
+  "q", 
+  "r", 
+  "s", 
+  "t", 
+  "u", 
+  "v", 
+  "w", 
+  "x", 
+  "y", 
+  "z"
+];
+var upperCaseAlphabet = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+var numberValue = [
+  0, 
+  1, 
+  2, 
+  3, 
+  4, 
+  5, 
+  6, 
+  7, 
+  8, 
+  9
+];
+var specialCharacters = [
+  "@", 
+  "%", 
+  "+", 
+  "/", 
+  "'", 
+  "!", 
+  "#", 
+  "$", 
+  "^", 
+  "?", 
+  ":", 
+  ",", 
+  "(", 
+  ")", 
+  "{", 
+  "}", 
+  "[", 
+  "]", 
+  "~", 
+  "`",
+  "_", 
+  "."
+]
+
+
+
+
 
 // generate the password based on user selections
 function generatePassword() {
-  var password = [];
   // promts the user to select a number between 8 and 128
-  passLength = prompt("How many characters would you like? Password must include at least 8 characters.");
-  // if the user picks a number between 8 and 128 then start password generator
-  if (passLength >= 8 && passLength <= 128){
-    // loops through the number of times the user picked
-    for (var i = 0; i <= (passLength - 1); i++) {
-      var valueNum = Math.floor (Math.random()*4);
-      //console.log("valueNum is " + valueNum);
-      if (valueNum === 0){
-        var alphNum = Math.floor (Math.random()* passValues.alphabet.length);
-        //console.log('alphNum is ' + alphNum);
-        passCharacter = passValues.alphabet[alphNum];
-        password.push([passCharacter]);
-        console.log(passCharacter);
-      } else if (valueNum === 1){
-        var alphNum = Math.floor (Math.random()* passValues.alphabet.length);
-        //console.log('alphNum is ' + alphNum);
-        passCharacter = passValues.alphabet[alphNum].toUpperCase();
-        password.push([passCharacter]);
-        console.log(passCharacter);
-      } else if (valueNum === 2){
-        var numbNum = Math.floor (Math.random()* passValues.number.length);
-        //console.log('Number is ' + numbNum);
-        passCharacter = passValues.number[numbNum];
-        password.push([passCharacter]);
-        console.log(passCharacter);
-      } else {
-        var specNum = Math.floor (Math.random()* passValues.specialChar.length);
-        //console.log('Number is ' + specNum);
-        passCharacter = passValues.specialChar[specNum];
-        password.push([passCharacter]);
-        console.log(passCharacter);
-      }
-      console.log("password is " + password);
-    }
-  } else {
-    // if the user didn't pick a number between 8 and 128 then ask again
-    alert("Password must be between 8 and 128 characters.")
-    writePassword();
-  }
-  return password
-}
+  var passLength = parseInt(
+    prompt("How many characters would you like? Password must include at least 8 characters.")
+  );
+  console.log(passLength);
 
+  // Checks proper length
+  if (passLength < 8 || passLength > 128){
+    alert('password needs to be between 8 and 128, please try again.')
+    return password = "Try Again";
+  } else if (isNaN(passLength)){
+    alert('password needs to be a number between 8 and 128, please try again.')
+    return password = "Try Again";
+  } else {
+    var useUpperCase = confirm (
+      'Would you like password to include upper case letters?'
+    );
+    var useLowerCase = confirm (
+      'Would you like password to include lower case letters?'
+    );
+    var useNumbers = confirm (
+      'Would you like password to include numbers?'
+    );
+    var useSpecialCharacters = confirm (
+      'Would you like password to include special characters?'
+    );
+    
+      if (
+        useUpperCase === false &&
+        useLowerCase === false &&
+        useNumbers === false &&
+        useSpecialCharacters === false
+      ) {
+        alert('password must include at least one type of character, please try again.')
+        return password = "Try Again";
+      }
+    
+    // Array to store possible options
+    var possibleOptions = [];
+    // Array to store useable options
+    var useOptions = [];
+
+    // If statement to store all the possible options
+    if (useLowerCase){
+      possibleOptions = possibleOptions.concat(lowCaseAlphabet);
+    }
+    if(useUpperCase){
+      possibleOptions = possibleOptions.concat(upperCaseAlphabet);
+    }
+    if(useNumbers){
+      possibleOptions = possibleOptions.concat(numberValue);
+    }
+    if(useSpecialCharacters){
+      possibleOptions = possibleOptions.concat(specialCharacters);
+    }
+    console.log(possibleOptions);
+    // loop to store all the useable characters
+    for (var i = 0; i <= (passLength -1); i++) {
+      var getRandom = Math.floor (Math.random ()* possibleOptions.length);
+      useOptions.push(possibleOptions[getRandom]);
+      console.log(useOptions);
+    }
+    console.log('useOptions is ' + useOptions);
+    password = useOptions;
+    console.log('password is ' + password);
+    return password;
+
+  
+   
+  }
+}
 
 
 // Write password to the #password input
